4b38678e2a51a004c666bc383c5049c82c63efdc
Nick Carter <thynctank@thynctank.com>
2009-07-14 11:36:31 -0400

    initialized repo

---
+++ storage.js
@@ -0,0 +1,6 @@
+var Storage = {
+  read: function(sql, success, failure) {},
+  write: function(sql, success, failure) {},
+  erase: function(sql, success, failure) {},
+  transact: function(func, success, failure) {}
+};
